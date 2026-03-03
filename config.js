454c2ad3fbef2e57393bf7d5caddc1354503b5cd
krit-hue <kritesh922@gmail.com>
2026-03-03 01:20:41 -0600

    Update webhook URL in config.js

--- config.js
+++ config.js
@@ -5,9 +5,10 @@
  */
 window.ORDER_BOT_CONFIG = {
   // Example: "https://hook.us1.make.com/xxxxx" or "https://n8n.yourdomain.com/webhook/orderbot"
-  WEBHOOK_URL: "https://hook.us2.make.com/l5vnxscozr3xei0stkphexgmptcjs3rr",
+  WEBHOOK_URL: "https://hook.us2.make.com/wa2vcgggy1pn53tl6oaf2vholgv3306d",
 
   // Set true to allow local demo lookup using orders.sample.json when webhook is empty/unavailable
   ALLOW_LOCAL_FALLBACK: true
 
 };
+
