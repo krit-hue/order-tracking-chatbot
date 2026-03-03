b588aef9928e7086a2128d09b5ceabd25fb3fd94
krit-hue <kritesh922@gmail.com>
2026-03-03 11:06:21 -0600

    Update RESULT_URL in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.ORDER_BOT_CONFIG = {
 
   // IMPORTANT: Set this after you create Scenario #2 (polling webhook)
   // Example: "https://hook.us2.make.com/xxxxxxxxxxxxxxxxxxxx"
-  RESULT_URL: "https://hook.us2.make.com/449a8ht3xgu5ewqmnobispr6iipyq8es",
+  RESULT_URL: "https://hook.us2.make.com/q7gk47n2dxc6dmzamiiyeocoalermfg2",
 
   POLL_INTERVAL_MS: 1500,
   POLL_TIMEOUT_MS: 25000,
