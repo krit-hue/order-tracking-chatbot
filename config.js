4cac794112125ff5d6f0d07b00f549bc443cd61e
krit-hue <kritesh922@gmail.com>
2026-03-04 06:23:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ORDER_BOT_CONFIG = {
   // Order Tracking (SYNC) – main webhook: POST message + sessionId, returns JSON with replyText/order
-  WEBHOOK_URL: "https://hook.us2.make.com/4a85s5hud5v76m4u5jm2eswqwiaqiqky",
+  WEBHOOK_URL: "https://hook.us2.make.com/xjehs9oupt992pvcva2v5rq1xhj161xs",
 
   // OrderTrack_Status – polling webhook: GET ?jobId=<sessionId>, returns JSON when result is ready
   RESULT_URL: "https://hook.us2.make.com/q7gk47n2dxc6dmzamiiyeocoalermfg2",
